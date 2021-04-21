3d221f5e62adddcbe411b7fa2e99ad048eb6b2ac
Malten <fmalten@stud.macromedia.de>
2021-04-21 15:53:43 +0200

    Player Movement & Camera Follow

--- Assets/C#/CharackterConroller.cs
+++ Assets/C#/CharackterConroller.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class CharackterConroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MovementSpeed = 2f;
+
+    private bool dashButtonDown;
+    private bool facingRight = true;
+    private Rigidbody2D rb;
+    Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dashButtonDown = true;
+        }
+
+        if (movement.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movement.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * MovementSpeed * Time.fixedDeltaTime);
+
+        if (dashButtonDown)
+        {
+            float dashAmount = 100f;
+            rb.MovePosition(rb.position + movement * Time.fixedDeltaTime * dashAmount);
+            dashButtonDown = false;
+        }
+    }
+
+    //flip the player
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the players x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
